5b17c4bd07596b4f9da41a61d02f255439b599b7
David H <lifeainteasy@web.de>
2021-07-25 01:00:39 +0200

    Putting HTML in Javascript

--- out/hello_world/core.cljs
+++ out/hello_world/core.cljs
@@ -1,11 +1,33 @@
 (ns hello-world.core)
 
 
-(println "Hello world!")
-
-
 (defn eval_question [id]
-  (js/console.log id)
+  (def myx "Hello world!2")
+  (js/console.log myx)
   (js/document.getElementById "app").innerHTML
   (.-innerHTML (.getElementById js/document "app"))
 )
+
+
+(defn create-heading-element [type text]
+  (->>
+   (.createTextNode js/document text)
+   (.appendChild (.createElement js/document tag type ) )
+   (.appendChild (.getElementById js/document "app"))
+)
+
+
+)
+
+(defn init []
+
+(create-heading-element "h1" "Logical Fallacies and Biases")
+(create-heading-element "h1" "Logical Fallacies and Biases")
+  )
+ ;  (def tag2 (.createElement js/document "button" ))
+ ;  (def text2 (.createTextNode js/document "Tutorix is the best e-learning platform"))
+ ;  (.appendChild tag2 text2)
+ ;  (.appendChild el tag2)
+ ;  )
+
+(init)
--- out/hello_world/core.js
+++ out/hello_world/core.js
@@ -1,14 +1,24 @@
 // Compiled by ClojureScript 1.10.520 {}
 goog.provide('hello_world.core');
 goog.require('cljs.core');
-cljs.core.println.call(null,"Hello world!");
 hello_world.core.eval_question = (function hello_world$core$eval_question(id){
-console.log(id);
+hello_world.core.myx = "Hello world!2";
+
+console.log(hello_world.core.myx);
 
 document.getElementById("app");
 
 
 return document.getElementById("app").innerHTML;
 });
+hello_world.core.create_heading_element = (function hello_world$core$create_heading_element(type,text){
+return document.getElementById("app").appendChild(document.createElement(hello_world.core.tag,type).appendChild(document.createTextNode(text)));
+});
+hello_world.core.init = (function hello_world$core$init(){
+hello_world.core.create_heading_element.call(null,"h1","Logical Fallacies and Biases");
+
+return hello_world.core.create_heading_element.call(null,"h1","Logical Fallacies and Biases");
+});
+hello_world.core.init.call(null);
 
 //# sourceMappingURL=core.js.map
